ecd4302b2b9dc65209d5d36c6aa4e299595235be
Amar Banerjee <amarbanerjee.ska@gmail.com>
2026-09-19 17:18:25 +0530

    PR20: define api v1 route contract

---
+++ com.kide.enterprise.api/src/com/kide/enterprise/api/ApiErrorCode.java
@@ -0,0 +1,13 @@
+package com.kide.enterprise.api;
+
+public enum ApiErrorCode {
+    BAD_REQUEST,
+    UNAUTHENTICATED,
+    FORBIDDEN,
+    NOT_FOUND,
+    CONFLICT,
+    TOO_LARGE,
+    RATE_LIMITED,
+    SERVICE_UNAVAILABLE,
+    INTERNAL_ERROR
+}
