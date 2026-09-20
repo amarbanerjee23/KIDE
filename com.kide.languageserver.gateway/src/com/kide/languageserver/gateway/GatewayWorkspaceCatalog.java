906f4e8d9b5c1ecdf9b4f0ff17e109dfdd5db8cc
Amar Banerjee <amarbanerjee.ska@gmail.com>
2026-09-20 12:03:26 +0530

    PR22 stabilization: resolve workspace IDs to canonical project bindings

--- com.kide.languageserver.gateway/src/com/kide/languageserver/gateway/GatewayWorkspaceCatalog.java
+++ com.kide.languageserver.gateway/src/com/kide/languageserver/gateway/GatewayWorkspaceCatalog.java
@@ -2,8 +2,6 @@ package com.kide.languageserver.gateway;
 
 import java.util.Optional;
 
-import com.kide.enterprise.context.EnterpriseContext;
-
 public interface GatewayWorkspaceCatalog {
-    Optional<EnterpriseContext> resolve(String workspaceId);
+    Optional<GatewayWorkspaceBinding> resolve(String workspaceId);
 }
