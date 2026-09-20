a481d5a38d83ba30c43916f86597d7e63a60834b
Amar Banerjee <amarbanerjee.ska@gmail.com>
2026-09-20 13:52:03 +0530

    PR27: remove invalid server-global rename binding

--- com.kide.languageserver/src/com/kide/languageserver/KideServerModules.java
+++ com.kide.languageserver/src/com/kide/languageserver/KideServerModules.java
@@ -1,6 +1,5 @@
 package com.kide.languageserver;
 
-import org.eclipse.xtext.ide.refactoring.IRenameStrategy2;
 import org.eclipse.xtext.ide.server.ServerModule;
 import org.eclipse.xtext.resource.IResourceServiceProvider;
 
@@ -21,7 +20,6 @@ public final class KideServerModules {
             @Override
             protected void configure() {
                 bind(IResourceServiceProvider.Registry.class).toProvider(registryProvider);
-                bind(IRenameStrategy2.class).to(IRenameStrategy2.DefaultImpl.class);
             }
         });
     }
