ead5d5fed6ac0bc3c282399681143998e134c109
Amar Banerjee <amarbanerjee.ska@gmail.com>
2026-09-22 17:20:03 +0530

    PR38: bind custom KRL validation layer

--- com.kide.krl.dsl/src/com/kide/krl/dsl/KrlRuntimeModule.java
+++ com.kide.krl.dsl/src/com/kide/krl/dsl/KrlRuntimeModule.java
@@ -4,7 +4,7 @@ import org.eclipse.xtext.formatting2.IFormatter2;
 import org.eclipse.xtext.validation.AbstractDeclarativeValidator;
 
 import com.kide.krl.dsl.formatting2.KrlFormatter;
-import com.kide.krl.dsl.validation.KrlValidator;
+import com.kide.krl.dsl.validation.KideKrlValidator;
 
 public class KrlRuntimeModule extends AbstractKrlRuntimeModule {
     public Class<? extends IFormatter2> bindIFormatter2() {
@@ -12,6 +12,6 @@ public class KrlRuntimeModule extends AbstractKrlRuntimeModule {
     }
 
     public Class<? extends AbstractDeclarativeValidator> bindAbstractDeclarativeValidator() {
-        return KrlValidator.class;
+        return KideKrlValidator.class;
     }
 }
