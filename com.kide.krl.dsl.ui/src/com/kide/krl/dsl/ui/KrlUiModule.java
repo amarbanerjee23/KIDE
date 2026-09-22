21f6266a38064651cde5ca3608ced78d0071b52e
Amar Banerjee <amarbanerjee.ska@gmail.com>
2026-09-22 16:38:51 +0530

    PR38: add generated Xtext KRL language skeleton

---
+++ com.kide.krl.dsl.ui/src/com/kide/krl/dsl/ui/KrlUiModule.java
@@ -0,0 +1,9 @@
+package com.kide.krl.dsl.ui;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+public class KrlUiModule extends AbstractKrlUiModule {
+    public KrlUiModule(AbstractUIPlugin plugin) {
+        super(plugin);
+    }
+}
