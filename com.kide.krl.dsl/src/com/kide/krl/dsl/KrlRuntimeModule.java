1929df21c7117e78a1887ebc08288e5a82355872
Amar Banerjee <amarbanerjee.ska@gmail.com>
2026-09-22 16:43:51 +0530

    PR38: bind packaged KRL formatter and validator

--- com.kide.krl.dsl/src/com/kide/krl/dsl/KrlRuntimeModule.java
+++ com.kide.krl.dsl/src/com/kide/krl/dsl/KrlRuntimeModule.java
@@ -3,6 +3,9 @@ package com.kide.krl.dsl;
 import org.eclipse.xtext.formatting2.IFormatter2;
 import org.eclipse.xtext.validation.AbstractDeclarativeValidator;
 
+import com.kide.krl.dsl.formatting2.KrlFormatter;
+import com.kide.krl.dsl.validation.KrlValidator;
+
 public class KrlRuntimeModule extends AbstractKrlRuntimeModule {
     public Class<? extends IFormatter2> bindIFormatter2() {
         return KrlFormatter.class;
