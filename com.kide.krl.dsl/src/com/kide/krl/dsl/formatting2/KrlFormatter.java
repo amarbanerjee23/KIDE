f5919a8f7d6b286603d89714f349af9e5bcdc40f
Amar Banerjee <amarbanerjee.ska@gmail.com>
2026-09-22 17:19:51 +0530

    PR38: match Xtext formatter visibility contract

--- com.kide.krl.dsl/src/com/kide/krl/dsl/formatting2/KrlFormatter.java
+++ com.kide.krl.dsl/src/com/kide/krl/dsl/formatting2/KrlFormatter.java
@@ -5,7 +5,7 @@ import org.eclipse.xtext.formatting2.IFormattableDocument;
 
 public final class KrlFormatter extends AbstractFormatter2 {
     @Override
-    protected void format(Object object, IFormattableDocument document) {
+    public void format(Object object, IFormattableDocument document) {
         // KRL generation semantics never depend on whitespace or formatter side effects.
     }
 }
