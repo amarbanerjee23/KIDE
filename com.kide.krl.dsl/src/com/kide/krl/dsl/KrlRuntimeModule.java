62e3d9f7a42195d576128e71fe1e4ba8d716053e
Amar Banerjee <amarbanerjee.ska@gmail.com>
2026-09-22 20:35:22 +0530

    PR38: delegate KRL validator binding to generated runtime module

--- com.kide.krl.dsl/src/com/kide/krl/dsl/KrlRuntimeModule.java
+++ com.kide.krl.dsl/src/com/kide/krl/dsl/KrlRuntimeModule.java
@@ -1,17 +1,10 @@
 package com.kide.krl.dsl;
 
 import org.eclipse.xtext.formatting2.IFormatter2;
-import org.eclipse.xtext.validation.AbstractDeclarativeValidator;
-
 import com.kide.krl.dsl.formatting2.KrlFormatter;
-import com.kide.krl.dsl.validation.KideKrlValidator;
 
 public class KrlRuntimeModule extends AbstractKrlRuntimeModule {
     public Class<? extends IFormatter2> bindIFormatter2() {
         return KrlFormatter.class;
-    }
-
-    public Class<? extends AbstractDeclarativeValidator> bindAbstractDeclarativeValidator() {
-        return KideKrlValidator.class;
     }
 }
