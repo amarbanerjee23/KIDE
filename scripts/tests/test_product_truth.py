272ebd3bfd409fd5724180fea9bbbcb43dd01c5a
Amar Banerjee <amarbanerjee.ska@gmail.com>
2026-09-22 16:56:30 +0530

    PR38: update product truth test for sixth DSL

--- scripts/tests/test_product_truth.py
+++ scripts/tests/test_product_truth.py
@@ -12,11 +12,11 @@ class ProductTruthTest(unittest.TestCase):
     def test_repository_product_truth_is_consistent(self):
         self.assertEqual([], verify_product_truth.verify_repository(ROOT))
 
-    def test_registry_is_exactly_five_unique_production_languages(self):
+    def test_registry_is_exactly_six_unique_production_languages(self):
         languages = verify_product_truth.load_registry(ROOT)
-        self.assertEqual(5, len(languages))
-        self.assertEqual(5, len({item["extension"] for item in languages}))
-        self.assertEqual(5, len({item["language_id"] for item in languages}))
+        self.assertEqual(6, len(languages))
+        self.assertEqual(6, len({item["extension"] for item in languages}))
+        self.assertEqual(6, len({item["language_id"] for item in languages}))
 
     def test_readme_block_is_deterministic(self):
         languages = verify_product_truth.load_registry(ROOT)
